e4cd6c2a4eb8021590b21ed8f1d57a7d9d605675
webwebweb5 <phiriyakornman@gmail.com>
2024-08-05 14:31:23 +0700

    ADD: question info dialog

--- src/sections/lab/main/lab-question-item.jsx
+++ src/sections/lab/main/lab-question-item.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 
 import {
@@ -6,20 +7,27 @@ import {
   Link,
   Stack,
   Button,
+  Dialog,
   Divider,
   MenuList,
   MenuItem,
   IconButton,
   Typography,
+  DialogTitle,
   ListItemText,
+  DialogContent,
+  DialogActions,
 } from '@mui/material';
 
 import { paths } from 'src/routes/paths';
 import { RouterLink } from 'src/routes/components';
 
+import Loading from 'src/app/loading';
 import { maxLine, varAlpha } from 'src/theme/styles';
+import { useGetALlTestCases } from 'src/actions/question';
 
 import { Iconify } from 'src/components/iconify';
+import { Markdown } from 'src/components/markdown';
 import { usePopover, CustomPopover } from 'src/components/custom-popover';
 
 // ----------------------------------------------------------------------
@@ -36,6 +44,69 @@ export function LabQuestionItem({ question, index }) {
   const router = useRouter();
 
   const { questionId, title, problemStatement, laboratory } = question;
+
+  const { testCases, testCasesLoading, testCasesError } = useGetALlTestCases(questionId);
+
+  const [questionInfoPopupOpen, setQuestionInfoPopupOpen] = useState(false);
+
+  if (testCasesLoading) return <Loading />;
+  if (testCasesError) return <Box>Something wrong</Box>;
+
+  const renderQuestionInfoDialog = (
+    <Dialog
+      fullWidth
+      maxWidth="sm"
+      open={questionInfoPopupOpen}
+      onClose={() => setQuestionInfoPopupOpen(false)}
+    >
+      <DialogTitle id="crop-dialog-title">Question Information</DialogTitle>
+      <DialogContent dividers style={{ position: 'relative' }}>
+        <Typography variant="h5" sx={{ mb: 2 }}>
+          {title}
+        </Typography>
+        <Markdown children={problemStatement} />
+        <Box
+          sx={{ my: 2 }}
+          gap={2}
+          display="grid"
+          gridTemplateColumns={{
+            xs: 'repeat(1, 1fr)',
+            sm: 'repeat(2, 1fr)',
+            md: 'repeat(3, 1fr)',
+          }}
+        >
+          {testCases?.map((testCase, i) => (
+            <Card key={i} sx={{ bgcolor: '#1B212A' }}>
+              <Stack spacing={0.5} sx={{ p: 2.5, px: 3, pr: 2.5 }}>
+                <Stack direction="row" alignItems="center" justifyContent="space-between">
+                  <Typography variant="h6">{`Test Case ${i + 1}`}</Typography>
+                </Stack>
+                <Divider sx={{ borderStyle: 'dashed', mb: 1 }} />
+                <Typography variant="body2">{`Input: ${testCase.input}`}</Typography>
+                <Typography variant="body2">{`Expected Output: ${testCase.expectedOutput}`}</Typography>
+              </Stack>
+            </Card>
+          ))}
+        </Box>
+      </DialogContent>
+
+      <DialogActions>
+        <Button
+          onClick={() => setQuestionInfoPopupOpen(false)}
+          startIcon={<Iconify icon="eva:close-outline" />}
+        >
+          Close
+        </Button>
+        <Button
+          color="primary"
+          variant="contained"
+          startIcon={<Iconify icon="carbon:play-filled-alt" />}
+        >
+          Test Lab
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 
   return (
     <>
@@ -69,7 +140,14 @@ export function LabQuestionItem({ question, index }) {
           <Stack spacing={1} sx={{ p: 3, pb: 2 }}>
             <ListItemText
               primary={
-                <Link component={RouterLink} href="" color="inherit">
+                <Link
+                  component={RouterLink}
+                  onClick={() => {
+                    setQuestionInfoPopupOpen(true);
+                  }}
+                  href=""
+                  color="inherit"
+                >
                   {title}
                 </Link>
               }
@@ -147,7 +225,7 @@ export function LabQuestionItem({ question, index }) {
         <MenuList>
           <MenuItem
             onClick={() => {
-              popover.onClose();
+              setQuestionInfoPopupOpen(true);
             }}
           >
             <Iconify icon="solar:eye-bold" />
@@ -174,6 +252,8 @@ export function LabQuestionItem({ question, index }) {
           </MenuItem>
         </MenuList>
       </CustomPopover>
+
+      {renderQuestionInfoDialog}
     </>
   );
 }
